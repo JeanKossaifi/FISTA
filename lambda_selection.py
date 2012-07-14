59677c83c7d9734ac71d54c7c37ae1dabdbacb86
JeanKossaifi <jean.kossaifi@gmail.com>
2012-07-14 02:01:00 +0200

    Corrected the cross validation to use correctly kernels

--- lambda_selection.py
+++ lambda_selection.py
@@ -12,18 +12,29 @@ import fista
 def _compute_mu(K, n_folds):
     return 1/norm(np.dot(K, K.transpose()), 2)
 
-def compute_score(estimator, K, y, lambda_, mus, n_folds, kf):
+def compute_score(estimator, K, y, lambda_, mus, n_folds, kf, n_samples, n_kernels):
     scores = np.zeros(n_folds)
     estimator.lambda_ = lambda_
     # Cross_validation loop
     for i, (train, test) in enumerate(kf):
-        scores[i] = estimator.fit(K[train, :], y[train, :], mus[i]).score(K[test, :], y[test, :])
+        train_col = list()
+        # little hack to have squared kernels
+        for i in range(n_kernels):
+            train_col.extend(train+n_samples*i)
+#        test_col = list()
+#        for i in range(n_kernels):
+#            test_col.extend(test+n_samples*i)
+        # computing the score
+        scores[i] = estimator.fit(K[train, :][:, train_col], y[train, :], mus[i], verbose=1).score(
+                K[test, :][:, train_col], y[test, :])
 
     return np.mean(scores)
 
 def select_lambda(penalty, lambdas, n_folds, K, y, K_name='default'):
     estimator = fista.Fista(penalty=penalty, n_iter=100)
     folds = KFold(K.shape[0], n_folds)
+    n_samples, n_features = K.shape
+    n_kernels = n_features / n_samples
     # Computing the coefficients mus for each fold
     print "** Computing the coefficients mus..."
     try:
@@ -38,9 +49,9 @@ def select_lambda(penalty, lambdas, n_folds, K, y, K_name='default'):
     print "\n\n** ... MUS : DONE\n\n"
     # Computing score mean score for each lambda
     print "** Computing scores ..."
-    scores = Parallel(n_jobs=4, verbose=2)\
+    scores = Parallel(n_jobs=1, verbose=2)\
             (delayed(compute_score)\
-            (clone(estimator), K, y, i, mus, n_folds, folds)\
+            (clone(estimator), K, y, i, mus, n_folds, folds, n_samples, n_kernels)\
              for i in lambdas) 
     result = Bunch()
     result['scores'] = dict(zip(lambdas, scores))
