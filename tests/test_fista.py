53c66ff193f8dc76e86832312bf64abb3c3d8280
JeanKossaifi <jean.kossaifi@gmail.com>
2012-07-15 15:29:18 +0200

    Added test function for norm_l12 and norm_l21

--- tests/test_fista.py
+++ tests/test_fista.py
@@ -6,7 +6,7 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from nose.tools import assert_true
 
-from ..fista import prox_l11, prox_l22, prox_l21, compute_M
+from ..fista import prox_l11, prox_l22, prox_l21, compute_M, norm_l12, norm_l21
 
 def test_prox_l11():
     u = np.ones(3)
@@ -40,3 +40,13 @@ def test_compute_M():
     Ml, sum_Ml = compute_M(u, l, len(u))
     assert_true(Ml==2)
     assert_true(sum_Ml==5)
+
+def test_norm_l12():
+    u = np.ones(8)
+    n_kernels, n_samples = 4, 2
+    assert norm_l12(u, n_samples, n_kernels) == 4
+
+def test_norm_l21():
+    u = np.ones(8)
+    n_kernels, n_samples = 2, 4
+    assert norm_l21(u, n_samples, n_kernels) == 4
